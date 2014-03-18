638dc4aff6fa9cc8b1ecef6f70490c1da7b9bec0
Janusz Slota <janusz.slota@nixilla.com>
2014-03-18 09:22:35 +0000

    Added listener priority parameter

--- src/ganja/component/event/Dispatcher.groovy
+++ src/ganja/component/event/Dispatcher.groovy
@@ -4,16 +4,33 @@ class Dispatcher {
 
     Map<String,List> listeners = [:]
 
-    void addListener(String eventName, Listener listener) {
+    void addListener(String eventName, Listener listener, Integer priority = 0) {
 
-        if(listeners[eventName])
-            listeners[eventName].add(listener)
-        else
-            listeners[eventName] = [ listener ]
+        if( ! listeners[eventName])
+            listeners[eventName] = []
+
+        listeners[eventName].add([ priority: priority, listener: listener ])
     }
 
     List getListeners(String eventName) {
 
-        listeners[eventName]
+        listeners[eventName]?.sort({ - it.priority })*.listener
+    }
+
+    Map getListeners() {
+
+        Map output = [:]
+
+        for(def entry in listeners)
+            output[entry.key] = getListeners(entry.key)
+
+        output
+    }
+
+
+
+    boolean hasListeners(String eventName) {
+
+        listeners[eventName]?.size() as Boolean
     }
 }
--- test/ganja/component/event/DispatcherSpec.groovy
+++ test/ganja/component/event/DispatcherSpec.groovy
@@ -10,8 +10,9 @@ class DispatcherSpec extends Specification {
         def dispatcher = new Dispatcher()
 
         then:
-
         dispatcher instanceof Dispatcher
+        dispatcher.getListeners() == [:]
+        ! dispatcher.hasListeners('any')
     }
 
     void "it can accept listener"() {
@@ -25,5 +26,38 @@ class DispatcherSpec extends Specification {
 
         then:
         dispatcher.getListeners('event.name') == [ listener ]
+        dispatcher.getListeners() == [ 'event.name': [ listener ]]
+        dispatcher.hasListeners('event.name')
+    }
+
+    void "it returns listeners sorted by priority"() {
+
+        given:
+        def dispatcher = new Dispatcher()
+
+        def listener1 = new Listener()
+        def listener2 = new Listener()
+        def listener3 = new Listener()
+        def listener4 = new Listener()
+        def listener5 = new Listener()
+        def listener6 = new Listener()
+
+        when:
+        dispatcher.addListener('foo', listener1, -10)
+        dispatcher.addListener('foo', listener2)
+        dispatcher.addListener('foo', listener3, 10)
+
+        dispatcher.addListener('bar', listener4, -10)
+        dispatcher.addListener('bar', listener5)
+        dispatcher.addListener('bar', listener6, 10)
+
+        then:
+        dispatcher.getListeners('foo') == [ listener3, listener2, listener1 ]
+        dispatcher.getListeners('bar') == [ listener6, listener5, listener4 ]
+
+        dispatcher.getListeners() == [
+                foo: [ listener3, listener2, listener1 ],
+                bar: [ listener6, listener5, listener4 ]
+            ]
     }
 }
